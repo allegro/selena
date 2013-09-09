a5966a1b5036e63bc9472cd298e87ab2ad30063e
Maciej Jagiellowicz <mjagiell@gmail.com>
2013-09-09 13:19:27 +0200

    pep8 fix

--- src/selena/stats/utils.py
+++ src/selena/stats/utils.py
@@ -7,7 +7,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from services.models import ResponseStateChoices as RSC
-from sys import stdout
 
 
 def get_real_problems_number(items, ok_mode=False,
